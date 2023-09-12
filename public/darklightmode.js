881e93c58812b19b74255763afd8f1a136d02e35
MarissaSmith <missmars1@gmail.com>
2023-09-11 23:05:49 -0500

    Added darklightmode.js styles.css

---
+++ public/darklightmode.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('myButton')
+const body = document.body // Get the body element
+
+// Add a click event listener to the button
+button.addEventListener('click', function () {
+  // Toggle between light and dark mode
+  if (body.classList.contains('dark-mode')) {
+    // Switch to light mode
+    body.classList.remove('dark-mode')
+  } else {
+    // Switch to dark mode
+    body.classList.add('dark-mode');
+  }
+})
