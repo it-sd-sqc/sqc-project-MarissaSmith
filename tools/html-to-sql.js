6c3a13b20525aada4258793a84d8712cfc37d659
MarissaSmith <missmars1@gmail.com>
2023-09-18 19:59:08 -0500

    Changed CSS Selectors

--- tools/html-to-sql.js
+++ tools/html-to-sql.js
@@ -4,24 +4,21 @@ import { parse } from 'node-html-parser';
 
 // Load and parse the HTML file
 const htmlFilePath = 'C:\\Users\\msmit\\OneDrive\\Documents\\CVTC\\SQC\\sqc-project-MarissaSmith\\data\\PrideandPrejudice.htm';
- // Update this path to your actual HTML file
-const html = readFileSync(htmlFilePath, 'utf8'); // Use readFileSync from the imported fs module
+ 
+const html = readFileSync(htmlFilePath, 'utf8'); 
 const root = parse(html);
-
-// The rest of your script remains unchanged
-
 
 // Select elements with the id "pg-header"
 const books = root.querySelectorAll('#pg-header');
 const chapters = root.querySelectorAll('a.pginternal');
-const texts = root.querySelectorAll('body'); // Update this selector to match your HTML structure
+const texts = root.querySelectorAll('body'); 
 
 // Process and generate SQL statements for Books
 let sqlStatementsBooks = '';
 books.forEach((book) => {
-  const bookTitle = book.querySelector('#pg-machine-header > p:nth-child(1) > strong:nth-child(1)').text; // Update this selector to match your HTML structure
-  const bookAuthor = book.querySelector('#pg-header-authlist > p:nth-child(1)').text; // Update this selector to match your HTML structure
-  const publicationDate = book.querySelector('.publication-date').text; // Update this selector to match your HTML structure
+  const bookTitle = book.querySelector('#pg-machine-header > p:nth-child(1) > strong:nth-child(1)').text; 
+  const bookAuthor = book.querySelector('#pg-header-authlist > p:nth-child(1)').text; 
+  const publicationDate = book.querySelector('#pg-machine-header > p:nth-child(3)').text; 
 
   const insertSql = `INSERT INTO Books (title, author, publication_date) VALUES ('${bookTitle}', '${bookAuthor}', '${publicationDate}');`;
   sqlStatementsBooks += insertSql + '\n';
@@ -30,8 +27,8 @@ books.forEach((book) => {
 // Process and generate SQL statements for Chapters
 let sqlStatementsChapters = '';
 chapters.forEach((chapter) => {
-  const chapterTitle = chapter.querySelector('.title').text; // Update this selector to match your HTML structure
-  const bookId = chapter.querySelector('.book-id').text; // Update this selector to match your HTML structure
+  const chapterTitle = chapter.querySelector('').text; 
+  const bookId = chapter.querySelector('').text; 
 
   const insertSql = `INSERT INTO Chapters (title, book_id) VALUES ('${chapterTitle}', ${bookId});`;
   sqlStatementsChapters += insertSql + '\n';
@@ -40,8 +37,8 @@ chapters.forEach((chapter) => {
 // Process and generate SQL statements for Texts
 let sqlStatementsTexts = '';
 texts.forEach((text) => {
-  const content = text.querySelector('.content').text; // Update this selector to match your HTML structure
-  const chapterId = text.querySelector('.chapter-id').text; // Update this selector to match your HTML structure
+  const content = text.querySelector('').text; 
+  const chapterId = text.querySelector('').text; 
 
   const insertSql = `INSERT INTO Texts (content, chapter_id) VALUES ('${content}', ${chapterId});`;
   sqlStatementsTexts += insertSql + '\n';
