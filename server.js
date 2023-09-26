a3e10a2acf5d1202cc9f21a131ca91d659e969e8
MarissaSmith <missmars1@gmail.com>
2023-09-25 22:29:18 -0500

    Update package files and index.html

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
-// Dependencies ////////////////////////////////////////////
-import express from 'express'
+// Dependencies ///////////////////////////////////////////
+import express from 'express';
 
-// Configuration ///////////////////////////////////////////
+// Configuration //////////////////////////////////////////
+const PORT = process.env.PORT || 5163;
+
+// Configure the web server ///////////////////////////////
+const app = express();
+
+// Set the 'views' directory and view engine (EJS)
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Routes /////////////////////////////////////////////////
+
+app.get('/', (req, res) => {
+  res.render('pages/index', { pageTitle: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about', { pageTitle: 'About' });
+});
+
+// Ready for browsers to connect //////////////////////////
+app.listen(PORT, () => {
+  console.log('Listening on ' + PORT);
+});
+
+
+
+/*// Configuration ///////////////////////////////////////////
 const PORT = 5163
 
 // Web server setup ////////////////////////////////////////
@@ -17,3 +47,4 @@ const displayPort = function () {
 
 // This line starts your server and makes it listen on port 5163.
 app.listen(PORT, displayPort)
+*/
