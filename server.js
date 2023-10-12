91a19ae695e53f7b2fbfafd9914624a1395248cd
kkeilholtz <143773724+kkeilholtz@users.noreply.github.com>
2023-10-12 18:06:31 -0500

    Added the views folder with all of the files for your index.ejs file, and also the shared header and footer for other .ejs pages. I also configured your server.js file to accept the index.ejs file

--- server.js
+++ server.js
@@ -1,33 +1,68 @@
 // Dependencies ///////////////////////////////////////////
-import express from 'express';
+import 'dotenv/config'
+import express from 'express'
+import pkg from 'pg'
+const { Pool } = pkg
 
 // Configuration //////////////////////////////////////////
 const PORT = process.env.PORT || 5163;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+export const query = async function (sql, params) {
+  let client
+  let results = []
+  try {
+    client = await pool.connect()
+    const response = client.query(sql, params)
+    if (response && response.rows) {
+      results = response.rows
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  if (client) client.release()
+  return results
+}
 
 // Configure the web server ///////////////////////////////
-const app = express();
+express()
+  .use(express.static('public'))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
 
-// Set the 'views' directory and view engine (EJS)
-app.set('views', 'views');
-app.set('view engine', 'ejs');
+  .set('views', 'views')
+  .set('view engine', 'ejs')
+.get('/', function (req, res) {
+  res.render('pages/index', {title: 'Home'})
+})
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
+.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// // Set the 'views' directory and view engine (EJS)
+// app.set('views', 'views');
+// app.set('view engine', 'ejs');
+
+// // Serve static files from the 'public' directory
+// app.use(express.static('public'));
 
 // Routes /////////////////////////////////////////////////
 
-app.get('/', (req, res) => {
-  res.render('pages/index', { pageTitle: 'Home' });
-});
+// app.get('/', (req, res) => {
+//   res.render('pages/index', { pageTitle: 'Home' });
+// });
 
-app.get('/about', (req, res) => {
-  res.render('pages/about', { pageTitle: 'About' });
-});
+// app.get('/about', (req, res) => {
+//   res.render('pages/about', { pageTitle: 'About' });
+// });
 
-// Ready for browsers to connect //////////////////////////
-app.listen(PORT, () => {
-  console.log('Listening on ' + PORT);
-});
+// // Ready for browsers to connect //////////////////////////
+// app.listen(PORT, () => {
+//   console.log('Listening on ' + PORT);
+// });
 
 
 
