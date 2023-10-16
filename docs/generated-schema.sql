e5e878e8506efcb67884bbcb8a2b70bad77e76a0
MarissaSmith <missmars1@gmail.com>
2023-10-16 17:59:51 -0500

    Updated Several Files

--- docs/generated-schema.sql
+++ docs/generated-schema.sql
@@ -1,14 +1,13 @@
--- Drop existing tables if they exist
 DROP TABLE IF EXISTS chapters CASCADE;
 DROP TABLE IF EXISTS books CASCADE;
 
--- Create the "Books" table
+-- Create the "books" table
 CREATE TABLE books (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL
 );
 
--- Create the "Chapters" table
+-- Create the "chapters" table
 CREATE TABLE chapters (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
